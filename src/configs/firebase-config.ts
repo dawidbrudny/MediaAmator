c7fc72d0096633353e6e2648163c5d7fb49f370d
Dawid Brudny <dawidbrudny.hq@gmail.com>
2024-08-17 20:55:23 +0200

    Add firebase-config to .gitignore

---
+++ src/configs/firebase-config.ts
@@ -0,0 +1,30 @@
+//  App
+import { initializeApp } from "firebase/app";
+
+//  Firestore, Auth, Storage
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from 'firebase/storage';
+import 'firebase/storage';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAvHmkodvHAk6EKExPPdAtP2LuAtb3HC3c",
+  authDomain: "mediaamator-779dd.firebaseapp.com",
+  projectId: "mediaamator-779dd",
+  storageBucket: "mediaamator-779dd.appspot.com",
+  messagingSenderId: "631001081841",
+  appId: "1:631001081841:web:ded24787c575965aa08650",
+  measurementId: "G-DQ4SPY64E7"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export { db, auth, storage };
